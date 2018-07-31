238ed2f83c9447df569c8f289bb2e14a0e2a8a7b
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-07-30 23:54:00 -0300

    sequences

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -6,7 +6,9 @@ FactoryBot.define do
     end
 
     name { Faker::Name.name }
-    email { Faker::Internet.email }
+    # email { Faker::Internet.email }
+
+    sequence(:email) { |n| "email#{n}@email.com"  }
 
     trait :female do
       gender 'F'
