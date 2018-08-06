ed1cd73de84dcf4f208b3be5a897a809a4f42962
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-08-06 13:39:05 -0300

    webmock

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,5 +1,7 @@
 describe 'HTTParty' do
   it 'return content-type application/json' do
+    stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+         to_return(status: 200, body: "", headers: { 'content-type': 'application/json' })
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
     expect(content_type).to match /application\/json/
