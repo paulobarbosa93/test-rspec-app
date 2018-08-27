e7e4129c0effb3073ed9f8f8865696a69ea9447b
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-08-26 23:09:25 -0300

    testing controllers (input of users)

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -1,16 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe CustomersController, type: :controller do
-  it 'responds successfully' do
-    get :index
-    expect(response).to be_success # have_http_status '200'
+  context "when not logged" do
+    it 'responds successfully' do
+      get :index
+      expect(response).to be_success # have_http_status '200'
+    end
   end
 
-  it '#show' do
-    member = create(:member)
-    customer = create(:customer)
-    sign_in member
-    get :show, params: { id: customer.id }
-    expect(response).to be_success
+  context "when logged" do
+    before do
+      @member = create(:member)
+      @customer = create(:customer)
+    end
+
+    it '#show' do
+      sign_in @member
+      get :show, params: { id: @customer.id }
+      expect(response).to be_success
+    end
+
+    it 'create with valid attributes' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      expect {
+        post :create, params: { customer: customer_params }
+      }.to change(Customer, :count).by(1)
+    end
   end
 end
