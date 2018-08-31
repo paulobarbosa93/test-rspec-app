b81be31e307eaf794aa408e8c45c366e999b3581
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-08-31 17:39:54 -0300

    page object pattern

--- spec/features/customers_spec.rb
+++ spec/features/customers_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require_relative '../support/new_customer_form'
 
 RSpec.feature "Customers", type: :feature, js: true do
   it 'Visit index page' do
@@ -19,18 +20,29 @@ RSpec.feature "Customers", type: :feature, js: true do
     expect(find('#my-div').find('h1')).to have_content 'Yes!'
   end
 
-  it 'Creates customer' do
-    member = create(:member)
-    login_as(member, scope: :member)
-
-    visit(new_customer_path)
-
-    fill_in('Name', with: Faker::Name.name)
-    fill_in('Email', with: Faker::Internet.email)
-    fill_in('Address', with: Faker::Address.street_address)
-
-    click_button('Create Customer')
+  it 'Creates customer with Page Object Pattern' do
+    new_customer_form = NewCustomerForm.new
+    new_customer_form.login.visit_page.fill_in_with(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      address: Faker::Address.street_address
+    ).submit
 
     expect(page).to have_content 'Customer was successfully created.'
   end
+
+  # it 'Creates customer' do
+  #   member = create(:member)
+  #   login_as(member, scope: :member)
+  #
+  #   visit(new_customer_path)
+  #
+  #   fill_in('Name', with: Faker::Name.name)
+  #   fill_in('Email', with: Faker::Internet.email)
+  #   fill_in('Address', with: Faker::Address.street_address)
+  #
+  #   click_button('Create Customer')
+  #
+  #   expect(page).to have_content 'Customer was successfully created.'
+  # end
 end
