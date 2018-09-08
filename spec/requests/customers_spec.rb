4161332c57d722fc03abb5daf14fad752c602680
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-09-08 18:56:02 -0300

    pure rspec

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -25,6 +25,14 @@ RSpec.describe "Customers", type: :request do
       )
     end
 
+    it 'show - Pure RSpec + JSON' do
+      get '/customers/1.json'
+      parsed_body = JSON.parse(response.body)
+      expect(parsed_body.fetch('id')).to eq 1
+      expect(parsed_body.fetch('name')).to be_kind_of(String)
+      expect(parsed_body.fetch('email')).to be_kind_of(String)
+    end
+
     it 'create - JSON' do
       member = create(:member)
       login_as(member, scope: :member)
