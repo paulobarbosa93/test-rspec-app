600cbc15250bc817093414ad05c07175488c6603
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-09-08 00:41:16 -0300

    post json

--- spec/requests/customers_spec.rb
+++ spec/requests/customers_spec.rb
@@ -2,19 +2,44 @@ require 'rails_helper'
 
 RSpec.describe "Customers", type: :request do
   describe "GET /customers" do
-    it "200 OK" do
+    it "index - JSON 200 OK" do
       get customers_path
       expect(response).to have_http_status(200)
     end
 
-    it 'index - JSON 200 OK' do
+    it 'index - JSON' do
       get '/customers.json'
-      expect(response).to have_http_status(200)
       expect(response.body).to include_json([
-        id: 1,
+        id: /\d/,
         name: (be_kind_of String),
         email: (be_kind_of String)
       ])
     end
+
+    it 'show - JSON' do
+      get '/customers/1.json'
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: (be_kind_of String),
+        email: (be_kind_of String)
+      )
+    end
+
+    it 'create - JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+
+      headers = { 'Accept' => 'application/json' }
+      customer_params = attributes_for(:customer)
+
+      post '/customers.json', params: { customer: customer_params }
+
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: customer_params.fetch(:name),
+        email: customer_params.fetch(:email)
+      )
+    end
+
   end
 end
